0cfa8ecdd49537715865022df5d4aa94f80bda25
Prem Kulkarni <pkulkarni@mediaocean.com>
2025-09-08 23:13:25 +0530

    Initialised sprint-boot service

---
+++ src/test/java/com/app/showboxd/ShowboxdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.showboxd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShowboxdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
